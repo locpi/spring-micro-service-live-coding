ad19958138b227e3e33da90dd5eab93a445bf74f
Loïc Pinçon <lpincon@ippon.Fr>
2022-04-27 10:21:30 +0200

    LPI - initial commit

---
+++ tips/src/main/java/Contact.java
@@ -0,0 +1,12 @@
+public class Contact {
+
+    private final String nom;
+
+    public Contact(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+}
